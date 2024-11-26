cdc7f2c174b917976b3e9d505c9cea0f04c9b0b5
vini-gln <157339675+vini-gln@users.noreply.github.com>
2024-11-25 23:17:40 -0300

    Update sw.js
    offline

--- sw.js
+++ sw.js
@@ -5,4 +5,46 @@ self.addEventListener('install', (event) => {
   self.addEventListener('fetch', (event) => {
     console.log('Requisição interceptada:', event.request.url);
   });
-  
+
+const CACHE_NAME = 'estoque-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+];
+
+// Instalar o Service Worker e armazenar os arquivos no cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Arquivos em cache');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Ativar o Service Worker e limpar caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Removendo cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar requisições e responder com cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
